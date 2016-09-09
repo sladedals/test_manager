302160926dc5f2cfa12f807b904076bf3ef9cb48
C.dric <sladedals@gmail.com>
2016-09-09 10:46:33 +0200

    with title ;)

--- testmngr/app/components/add-test.js
+++ testmngr/app/components/add-test.js
@@ -3,11 +3,9 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   store: Ember.inject.service(),
   actions: {
-    createTest() {
+    createTest(item) {
       var store = this.get('store');
-      var name = $('#name').val();
-      console.log("here am i " + name);
-      let test = store.createRecord('test-case', {title: 'for_test', status: store.peekRecord('status',4)});
+      let test = store.createRecord('test-case', {title: item.get('title'), status: store.peekRecord('status',4)});
       test.save();
     }
   }
